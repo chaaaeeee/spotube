f8d9daae25522831331601ee696e0c7c6c58c245
chaaaeeee <mash1o1o1o1@gmail.com>
2024-06-01 15:52:29 +0700

    add google oauth2

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.3
 require github.com/zmb3/spotify/v2 v2.4.2
 
 require (
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/oauth2 v0.20.0
 	google.golang.org/api v0.181.0
 )
--- internal/youtube/auth.go
+++ internal/youtube/auth.go
@@ -4,12 +4,20 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
 	"net/http"
+	"os"
 )
+
+type OAuth2Config struct {
+	ClientID     string
+	ClientSecret string
+	RedirectURL  string
+}
 
 type Service struct {
 	youtubeService *youtube.Service
@@ -39,8 +47,14 @@ func NewService(r *http.Request) (*Service, error) {
 }
 
 func SetupConfig() *oauth2.Config {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
 	conf := &oauth2.Config{
-		RedirectURL: "http://localhost:8080/google/callback",
+		ClientID:     os.Getenv("ClientID"),
+		ClientSecret: os.Getenv("ClientSecret"),
+		RedirectURL:  os.Getenv("RedirectURL"),
 		Scopes: []string{
 			"https://www.googleapis.com/auth/youtube",
 			"https://www.googleapis.com/auth/youtube.force-ssl",
